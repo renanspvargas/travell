0cde836da191289574fc605f739768f1312e7695
renan vargas <renanspvargas@outlook.com>
2022-11-28 19:41:06 -0300

    create class for assets

---
+++ lib/assets/images.dart
@@ -0,0 +1,13 @@
+class Images {
+  static const _path = "assets/images/";
+
+  static const balloning = "${_path}balloning.png";
+  static const buttonOne = "${_path}button-one.png";
+  static const hiking = "${_path}hiking.png";
+  static const kayaking = "${_path}kayaking.png";
+  static const mountain = "${_path}mountain.png";
+  static const snorkling = "${_path}snorkling.png";
+  static const welcomeOne = "${_path}welcome-one.png";
+  static const welcomeTwo = "${_path}welcome-two.png";
+  static const welcomeThree = "${_path}welcome-three.png";
+}
