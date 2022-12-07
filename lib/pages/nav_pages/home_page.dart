c6e8a642b2945399999843813676ad33a3187023
Renan Vargas <renanspvargas@outlook.com>
2022-12-07 09:38:40 -0300

    home page header

--- lib/pages/nav_pages/home_page.dart
+++ lib/pages/nav_pages/home_page.dart
@@ -10,6 +10,35 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 70, left: 20, right: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Icon(Icons.menu, size: 30, color: Colors.black),
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.withOpacity(0.5),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(height: 40),
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: const AppLargeText(text: "Discover"),
+          ),
+          const SizedBox(height: 30),
+        ],
+      ),
+    );
   }
 }
