aac41e392a465aa1881ac47af1ae3c1a118393f0
renanspvargas <renanspvargas@outlook.com>
2022-12-07 20:03:19 -0300

    adding text and app button to detail screen

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -10,14 +10,130 @@ class DetailPage extends StatefulWidget {
 }
 
 class _DetailPageState extends State<DetailPage> {
+  int _gottenStars = 4;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
+      body: SizedBox(
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         child: Stack(
-          children: [],
+          children: [
+            Positioned(
+              left: 0,
+              right: 0,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 350,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(Images.mountain),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              left: 20,
+              top: 70,
+              child: Row(
+                children: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.menu),
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              top: 320,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 500,
+                padding: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        AppLargeText(
+                          text: "Yosemite",
+                          color: Colors.black.withOpacity(0.8),
+                        ),
+                        const AppLargeText(
+                          text: "\$ 250",
+                          color: AppColors.mainColor,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: const [
+                        Icon(Icons.location_on, color: AppColors.mainColor),
+                        SizedBox(width: 5),
+                        AppText(
+                          text: "USA, California",
+                          color: AppColors.textColor1,
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Wrap(
+                          children: List.generate(5, (index) {
+                            return Icon(
+                              Icons.star,
+                              color: index < _gottenStars
+                                  ? AppColors.starColor
+                                  : AppColors.textColor2,
+                            );
+                          }),
+                        ),
+                        const SizedBox(width: 10),
+                        const AppText(
+                          text: "(4.0)",
+                          color: AppColors.textColor2,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 25),
+                    AppLargeText(
+                      text: "People",
+                      color: Colors.black.withOpacity(0.8),
+                      size: 20,
+                    ),
+                    const SizedBox(height: 5),
+                    const AppText(
+                      text: "Number of people in your group",
+                      color: AppColors.mainTextColor,
+                    ),
+                    Wrap(
+                      children: List.generate(5, (index) {
+                        return AppButton(
+                          color: Colors.black,
+                          backgroundColor: AppColors.buttonBackground,
+                          size: 50,
+                          borderColor: AppColors.buttonBackground,
+                          child: Text("${index + 1}"),
+                        );
+                      }),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
