7384102236b88b44e1050721cbf16fcc30fb46a7
renanspvargas <renanspvargas@outlook.com>
2022-12-07 20:38:29 -0300

    configuring component and adding validations

--- lib/widgets/app_button.dart
+++ lib/widgets/app_button.dart
@@ -1,30 +1,51 @@
 import 'package:travell/imports.dart';
 
 class AppButton extends StatelessWidget {
-  final Widget child;
   final Color color;
   final Color backgroundColor;
   final double size;
   final Color borderColor;
+  late IconData? icon;
+  late String? text;
 
-  const AppButton(
-      {super.key,
-      required this.child,
-      required this.color,
-      required this.backgroundColor,
-      required this.size,
-      required this.borderColor});
+  AppButton({
+    super.key,
+    required this.color,
+    required this.backgroundColor,
+    required this.size,
+    required this.borderColor,
+    this.icon,
+    this.text,
+  })  : assert(icon != null && text != null, "You must provide icon OR text"),
+        assert(icon == null && text == null,
+            "Cannot provide both a icon and a text at the same time");
+
+  Widget getCenteredWidget() {
+    if (icon != null) {
+      return Icon(icon, color: color);
+    }
+
+    if (text != null) {
+      return Text(
+        text!,
+        style: TextStyle(color: color),
+      );
+    }
+
+    return Container();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 60,
-      height: 60,
+      width: size,
+      height: size,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
-        color: AppColors.buttonBackground,
+        color: backgroundColor,
+        border: Border.all(color: borderColor, width: 1.0),
       ),
-      child: Center(child: child),
+      child: Center(child: getCenteredWidget()),
     );
   }
 }
