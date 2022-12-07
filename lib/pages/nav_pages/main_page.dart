d6f482ea2fb11b214fc19563a01f696d89f0307b
Renan Vargas <renanspvargas@outlook.com>
2022-12-07 09:08:40 -0300

    creating main page

---
+++ lib/pages/nav_pages/main_page.dart
@@ -0,0 +1,32 @@
+import 'package:travell/imports.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  List pages = [
+    const HomePage(),
+    const BarItemPage(),
+    const SearchPage(),
+    const MyPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.apps)),
+          BottomNavigationBarItem(
+              label: "Bar", icon: Icon(Icons.bar_chart_sharp)),
+          BottomNavigationBarItem(label: "Search", icon: Icon(Icons.search)),
+          BottomNavigationBarItem(label: "My", icon: Icon(Icons.person)),
+        ],
+      ),
+    );
+  }
+}
