cce6c72611e845c7d712cb85942622138a1f6dd4
Renan Vargas <renanspvargas@outlook.com>
2022-12-07 11:45:41 -0300

    adding explore section

--- lib/pages/nav_pages/home_page.dart
+++ lib/pages/nav_pages/home_page.dart
@@ -13,90 +13,139 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     TabController _tabController = TabController(length: 3, vsync: this);
 
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.only(top: 70, left: 20, right: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Icon(Icons.menu, size: 30, color: Colors.black),
-                Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.grey.withOpacity(0.5),
-                  ),
-                )
-              ],
-            ),
-          ),
-          const SizedBox(height: 40),
-          Container(
-            margin: const EdgeInsets.only(left: 20),
-            child: const AppLargeText(text: "Discover"),
-          ),
-          const SizedBox(height: 30),
-          Container(
-            // margin: const EdgeInsets.only(left: 20),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: TabBar(
-                labelColor: Colors.black,
-                unselectedLabelColor: Colors.grey,
-                labelPadding: const EdgeInsets.only(left: 20, right: 20),
-                indicatorSize: TabBarIndicatorSize.label,
-                indicator: const CircleTabIndicator(
-                  color: AppColors.mainColor,
-                  radius: 4,
-                ),
-                isScrollable: true,
-                controller: _tabController,
-                tabs: const [
-                  Tab(text: "Places"),
-                  Tab(text: "Inspiration"),
-                  Tab(text: "Emotions"),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 70, left: 20, right: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(Icons.menu, size: 30, color: Colors.black),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.withOpacity(0.5),
+                    ),
+                  )
                 ],
               ),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(left: 20),
-            height: 300,
-            width: MediaQuery.of(context).size.width,
-            child: TabBarView(
-              controller: _tabController,
-              children: [
-                ListView.builder(
-                  itemCount: 3,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(
-                        horizontal: 5,
-                        vertical: 10,
-                      ),
-                      width: 200,
-                      height: 300,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.white,
-                        image: const DecorationImage(
-                          image: AssetImage(Images.mountain),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-                const Text("tab1"),
-                const Text("tab2"),
-              ],
+            const SizedBox(height: 40),
+            Container(
+              margin: const EdgeInsets.only(left: 20),
+              child: const AppLargeText(text: "Discover"),
             ),
-          )
-        ],
+            const SizedBox(height: 30),
+            Container(
+              // margin: const EdgeInsets.only(left: 20),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: TabBar(
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey,
+                  labelPadding: const EdgeInsets.only(left: 20, right: 20),
+                  indicatorSize: TabBarIndicatorSize.label,
+                  indicator: const CircleTabIndicator(
+                    color: AppColors.mainColor,
+                    radius: 4,
+                  ),
+                  isScrollable: true,
+                  controller: _tabController,
+                  tabs: const [
+                    Tab(text: "Places"),
+                    Tab(text: "Inspiration"),
+                    Tab(text: "Emotions"),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 20),
+              height: 300,
+              width: MediaQuery.of(context).size.width,
+              child: TabBarView(
+                controller: _tabController,
+                children: [
+                  ListView.builder(
+                    itemCount: 3,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 5,
+                          vertical: 10,
+                        ),
+                        width: 200,
+                        height: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          image: const DecorationImage(
+                            image: AssetImage(Images.mountain),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  const Text("tab1"),
+                  const Text("tab2"),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  AppLargeText(text: "Explore more", size: 22),
+                  AppText(text: "See all", color: AppColors.textColor1)
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              height: 120,
+              width: MediaQuery.of(context).size.width,
+              margin: const EdgeInsets.only(left: 20),
+              child: ListView.builder(
+                itemCount: 4,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext, int) {
+                  return Container(
+                    margin: const EdgeInsets.only(right: 30),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 80,
+                          height: 80,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            image: const DecorationImage(
+                              image: AssetImage(Images.mountain),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const AppText(
+                          text: "Kayaking",
+                          color: AppColors.textColor2,
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
