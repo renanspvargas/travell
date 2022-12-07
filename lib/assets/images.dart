dd5339d7c4bbc26f462cdc15e8cbb4181029aa43
Renan Vargas <renanspvargas@outlook.com>
2022-12-07 11:00:40 -0300

    correcting image reference

--- lib/assets/images.dart
+++ lib/assets/images.dart
@@ -5,7 +5,7 @@ class Images {
   static const buttonOne = "${_path}button-one.png";
   static const hiking = "${_path}hiking.png";
   static const kayaking = "${_path}kayaking.png";
-  static const mountain = "${_path}mountain.png";
+  static const mountain = "${_path}mountain.jpeg";
   static const snorkling = "${_path}snorkling.png";
   static const welcomeOne = "${_path}welcome-one.png";
   static const welcomeTwo = "${_path}welcome-two.png";
