25308549edd69d4cdc608fedc5e50ec6c34bd96d
Renan Vargas <renanspvargas@outlook.com>
2022-12-07 10:51:30 -0300

    configuring indicator position

--- lib/extensions/circle_tab_indicator.dart
+++ lib/extensions/circle_tab_indicator.dart
@@ -25,6 +25,15 @@ class _CirclePainter extends BoxPainter {
     _paint.color = color;
     _paint.isAntiAlias = true;
 
+    final Size? configSize = configuration.size;
+
+    if (configSize != null) {
+      Offset circleOffset =
+          Offset(configSize.width / 2 - radius / 2, configSize.height - radius);
+      canvas.drawCircle(offset + circleOffset, radius, _paint);
+      return;
+    }
+
     canvas.drawCircle(offset, radius, _paint);
   }
 }
